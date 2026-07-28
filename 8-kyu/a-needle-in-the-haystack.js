a14fd82276f8507140738fc67deee8e0abb13ce5
Josh Yi <joshua.yi1018@gmail.com>
2026-07-28 18:23:44 -0400

    Removed haystack

--- 8-kyu/a-needle-in-the-haystack.js
+++
@@ -1,8 +0,0 @@
-function findNeedle(haystack) {
-  // your code here
-  for (let i=0; i<haystack.length; i++){
-    if (haystack[i] === 'needle'){
-      return `found the needle at position ${i}`
-    }
-  }
-}
