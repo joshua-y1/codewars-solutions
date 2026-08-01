def72b54413b556da7fbaa484ce4522915ee2c1b
Josh Yi <joshua.yi1018@gmail.com>
2026-08-01 14:18:46 -0400

    Solve 8 kyu school paperwork

---
+++ 8-kyu/school-paperwork.js
@@ -0,0 +1,7 @@
+// Beginner Series #1 School Paperwork
+// 8 kyu
+// https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd/solutions/javascript
+
+function paperwork(n, m) {
+  return n<0 || m<0 ? 0 : n * m
+}
