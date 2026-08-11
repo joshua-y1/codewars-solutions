db93e82c8c53f054944a5d432e721010738deb60
Josh Yi <joshua.yi1018@gmail.com>
2026-08-11 13:10:00 -0400

    Solve 8 kyu set alarm

---
+++ 8-kyu/set-alarm.js
@@ -0,0 +1,7 @@
+// L1: Set Alarm
+// 8 kyu
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/javascript
+
+function setAlarm(employed, vacation){
+  return employed && !vacation ? true : false;
+}
