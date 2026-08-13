af592dad8ec9ad330b2f18e3fd664fdf13991d54
Josh Yi <joshua.yi1018@gmail.com>
2026-08-13 13:45:06 -0400

    Solve 8 kyu smallest integer

---
+++ 8-kyu/find-the-smallest-integer-in-array.js
@@ -0,0 +1,7 @@
+// Find the smallest integer in the array
+// 8 kyu
+// https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/javascript
+
+function findSmallestInt(arr) {
+    return Math.min(...arr);
+}
