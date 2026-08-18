a18c039c7a9a19d56c16c38c559e31065f7e9a86
Josh Yi <joshua.yi1018@gmail.com>
2026-08-18 12:47:44 -0400

    Solve 7 kyu binary addition

---
+++ 7-kyu/binary-addition.js
@@ -0,0 +1,8 @@
+// Binary Addition
+// 7 Kyu
+// https://www.codewars.com/kata/551f37452ff852b7bd000139/train/javascript
+
+function addBinary(a,b) {
+  let sum = a + b;
+  return sum.toString(2);
+}
