2000fbff7c7933dd59bd7ffe42bd7f3a92f54637
Josh Yi <joshua.yi1018@gmail.com>
2026-08-21 13:33:35 -0400

    Solve 8 kyu convert boolean to string

---
+++ 8-kyu/convert-boolean-to-string.js
@@ -0,0 +1,7 @@
+// Convert boolean values to strings 'Yes' or 'No'
+// 8 Kyu
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+function boolToWord( bool ){
+  return bool ? "Yes" : "No";
+}
