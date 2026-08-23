0b8208558fa184a1f0e67b5a0713dc3240a88263
Josh Yi <joshua.yi1018@gmail.com>
2026-08-23 14:13:19 -0400

    Solve 8 kyu sum of positive

---
+++ 8-kyu/sum_of_positive.py
@@ -0,0 +1,12 @@
+# Sum of Positive
+# 8 Kyu
+# https://www.codewars.com/kata/5715eaedb436cf5606000381/train/python
+
+def positive_sum(arr):
+    sum = 0
+    for num in arr:
+        if num > 0:
+            sum += num
+        else:
+            continue
+    return sum
