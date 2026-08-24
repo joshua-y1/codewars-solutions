9adcf83ffd64af1e9880f3ab470789b65a6585c2
Josh Yi <joshua.yi1018@gmail.com>
2026-08-24 12:59:02 -0400

    Solve 8 kyu convert a boolean to string

---
+++ 8-kyu/convert_a_boolean_to_string.py
@@ -0,0 +1,10 @@
+# Convert a Boolean to a String
+# 8 Kyu
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+
+def boolean_to_string(b):
+    if b == True:
+        b = 'True'
+    elif b == False:
+        b = 'False'
+    return b
