d51843cc98bf9e8799e119baa81a477c47577582
Josh Yi <joshua.yi1018@gmail.com>
2026-08-28 14:52:24 -0400

    Solve 8 kyu surface area and volume of a box and convert string to number

---
+++ 8-kyu/convert-a-string-to-a-number.py
@@ -0,0 +1,7 @@
+# Convert a String to a Number 
+# 8 Kyu
+# https://www.codewars.com/kata/544675c6f971f7399a000e79/train/python
+
+def string_to_number(s):
+    return int(s)
+
---
+++ 8-kyu/surface-area-and-voume-of-a-box.sql
@@ -0,0 +1,12 @@
+-- Surface Area and Volume of a Box
+-- 8 Kyu
+-- https://www.codewars.com/kata/565f5825379664a26b00007c/train/sql
+
+-- # write your SQL statement here: 
+-- you are given a table 'box' with columns: width (int), height (int), depth (int)
+-- return a query with columns: width, height, depth, area (int), volume (int)
+-- sort results by area ascending, then volume ascending, then width ascending, then height ascending
+
+SELECT width, height, depth, 2*((width*depth)+ (width*height) + (depth*height)) AS area, width * height * depth AS volume 
+FROM box
+ORDER BY area, volume, width, height ASC
