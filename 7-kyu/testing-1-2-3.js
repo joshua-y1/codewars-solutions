2905d09f43ff9e25ca2a086817c8e2985dc8c9f0
Josh Yi <joshua.yi1018@gmail.com>
2026-08-29 18:03:50 -0400

    Solve 7 kyu testing 1 2 3

---
+++ 7-kyu/testing-1-2-3.js
@@ -0,0 +1,7 @@
+// Testing 1-2-3
+// 7 Kyu
+// https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/solutions/javascript
+
+var number=function(array){
+  return array.map((x,i) => `${i + 1}: ${x}`);
+}
