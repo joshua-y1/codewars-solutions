01cb9a2270babb4b6ea1e22c69a8cf396cfaf5a0
Josh Yi <joshua.yi1018@gmail.com>
2026-09-09 16:48:04 -0400

    Solve 8 kyu beginner series 2 clock

---
+++ 8-kyu/beginner-series-2-clock.js
@@ -0,0 +1,11 @@
+// Beginner Series #2 Clock
+// 8 Kyu
+// https://www.codewars.com/kata/55f9bca8ecaa9eac7100004a/train/javascript
+
+function past(h, m, s){
+  //#Happy Coding! ^_^
+  h *= 3600000;
+  m *= 60000;
+  s *= 1000;
+  return h + m + s; 
+}
