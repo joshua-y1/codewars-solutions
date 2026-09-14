f93accc34b5657086c680a58f6091ad2d75b94d2
Josh Yi <joshua.yi1018@gmail.com>
2026-09-14 12:32:58 -0400

    Solve 7 kyu the highest profit wins

---
+++ 7-kyu/the-highest-profit-wins.js
@@ -0,0 +1,7 @@
+// The Highest Profit Wins!
+// 7 Kyu
+// https://www.codewars.com/kata/559590633066759614000063/train/javascript
+
+function minMax(arr){
+  return [Math.min(...arr),Math.max(...arr)]
+}
