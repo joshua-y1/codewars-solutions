31cbb8996f0d1f43f8c2c8ee4beedbac30c2e49d
Josh Yi <joshua.yi1018@gmail.com>
2026-09-19 19:34:06 -0400

    Solve 7 kyu list filtering

---
+++ 7-kyu/list-filtering.js
@@ -0,0 +1,7 @@
+// List Filtering
+// 7 Kyu
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/javascript
+
+function filter_list(l) {
+  return l.filter((x) => typeof x === "number")
+}
