e5a21ba0415f400f97ce1bcd2140ef4396530c4d
Josh Yi <joshua.yi1018@gmail.com>
2026-09-21 12:23:54 -0400

    Solve 8 kyu is he gonna survive

---
+++ 8-kyu/is-he-gonna-survive.js
@@ -0,0 +1,9 @@
+// Is He Gonna Survive?
+// 8 Kyu 
+// https://www.codewars.com/kata/59ca8246d751df55cc00014c/train/javascript
+
+function hero(bullets, dragons){
+  return bullets - (dragons * 2) >= 0
+}
+ 
+ 
